382e62cab6f12da988d3ec7057f1d67d410fcb40
Ethan Nguyen <ethan.nguyen@ou.edu>
2023-02-14 18:44:51 -0600

    Created sumList function

--- SE-Assignment3/SE-Assignment3.cpp
+++ SE-Assignment3/SE-Assignment3.cpp
@@ -2,10 +2,28 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
+int sumList(vector<int> data) {
+	// Add all of the numbers in an array
+	int sum = 0;
+	for (int i = 0; i < data.size(); i++) {
+		sum += data.at(i);
+	}
+	return sum;
+}
+
 int main()
 {
+	vector <int> data;
+	data.push_back(5);
+	data.push_back(7);
+	data.push_back(4);
+	data.push_back(9);
+	data.push_back(3);
 
-	cout << "Hello World!";
+	int sum = sumList(data);
+
+	cout << sum << endl;
 
 }
